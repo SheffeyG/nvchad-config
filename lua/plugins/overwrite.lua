8ccf1f986677522d7297d25b327c73255a39c434
sheffey <57262511+SheffeyG@users.noreply.github.com>
2024-05-02 18:43:03 +0800

    Split custom plugins config

--- lua/plugins/overwrite.lua
+++ lua/plugins/overwrite.lua
@@ -39,13 +39,4 @@ return {
   -- 		},
   -- 	},
   -- },
-
-  -- Custom plugins
-  {
-    "max397574/better-escape.nvim",
-    event = "InsertEnter",
-    config = function()
-      require("better_escape").setup()
-    end,
-  },
 }
