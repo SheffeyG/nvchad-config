99555c7bbee66526a1e11a749eb6914d873dd9de
sheffey <57262511+SheffeyG@users.noreply.github.com>
2024-05-03 01:27:16 +0800

    Make clang-format tabwidth 4

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -1,7 +1,7 @@
 local options = {
   formatters_by_ft = {
-    c = { "clangd" },
-    cpp = { "clangd" },
+    c = { "clang_fmt" },
+    cpp = { "clang_fmt" },
     css = { "prettier" },
     javascript = { "prettier" },
     html = { "prettier" },
@@ -16,13 +16,9 @@ local options = {
   --   timeout_ms = 500,
   --   lsp_fallback = true,
   -- },
-
-  -- formatters = {
-  --   clangdf = {
-  --     command = { "clangd-format" },
-  --     -- args = { "-style=\"{UseTab: Never, IndentWidth: 4, TabWidth: 4}\"" },
-  --   },
-  -- },
 }
+
+require("conform").formatters.clang_fmt = vim.deepcopy(require("conform.formatters.clang-format"))
+require("conform").formatters.clang_fmt.prepend_args = {'-style="{IndentWidth: 4}"'}
 
 require("conform").setup(options)
