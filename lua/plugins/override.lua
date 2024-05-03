6101d97eb60761351ec03be7a256c1ff8e265e54
sheffey <57262511+SheffeyG@users.noreply.github.com>
2024-05-03 17:26:00 +0800

    Disable default lspconfig with lua

--- lua/plugins/override.lua
+++ lua/plugins/override.lua
@@ -4,7 +4,7 @@ return {
   {
     "neovim/nvim-lspconfig",
     config = function()
-      require("nvchad.configs.lspconfig").defaults()
+      -- require("nvchad.configs.lspconfig").defaults()
       require("configs.lspconfig")
     end,
   },
