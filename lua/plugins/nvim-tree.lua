c45a5f94261242abf8df207627945f2d8b0dd6a5
sheffey <57262511+SheffeyG@users.noreply.github.com>
2024-05-20 19:14:16 +0800

    override nvtree and devicons for termux

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,85 +1,81 @@
-if vim.fn.has("termux") ~= 1 then
-    return {
-        -- "nvim-tree/nvim-tree.lua",
-        -- config = function()
-        --     require("nvim-tree").setup({
-        --         renderer = {
-        --             icons = {
-        --                 git_placement = "after",
-        --                 -- padding = "",
-        --                 symlink_arrow = " ➜ ",
-        --             }
-        --         }
-        --     })
-        -- end
+local nvtree_opts = {
+    renderer = {
+        icons = {
+            git_placement = "after",
+            symlink_arrow = "➜",
+        },
+    },
+}
+local devicons = require("nvchad.icons.devicons")
+
+if vim.fn.has("termux") == 1 then
+    -- override some nvtree opts for termux
+    nvtree_opts = {
+        renderer = {
+            icons = {
+                git_placement = "after",
+                padding = "",
+                symlink_arrow = "➜",
+                glyphs = {
+                    default = "󰈚 ",
+                    symlink = " ",
+                    folder = {
+                        default = " ",
+                        empty = " ",
+                        empty_open = " ",
+                        open = " ",
+                        symlink = " ",
+                        symlink_open = " ",
+                        arrow_open = "",
+                        arrow_closed = "",
+                    },
+                    git = {
+                        unstaged = "✗",
+                        staged = "✓",
+                        unmerged = "",
+                        renamed = "➜",
+                        untracked = "★",
+                        deleted = "-",
+                        ignored = "◌",
+                    },
+                },
+            },
+        },
     }
-end
 
--- Fix termux nvtree icons
-local nvtree_devicons = require("nvim-web-devicons").get_icons()
-local nvchad_devicons = require("nvchad.icons.devicons")
-local override = {}
+    -- override all devicons for termux
+    local nvtree_devicons = require("nvim-web-devicons").get_icons()
+    local nvchad_devicons = require("nvchad.icons.devicons")
 
-for name, data in pairs(nvtree_devicons) do
-    local icon = data.icon
-    local pad = vim.api.nvim_strwidth(icon) == 1 and " " or ""
-    override[name] = vim.tbl_extend("force", data, { icon = icon .. pad })
-end
+    for name, data in pairs(nvtree_devicons) do
+        local icon = data.icon
+        local pad = vim.api.nvim_strwidth(icon) == 1 and " " or ""
+        devicons[name] = vim.tbl_extend("force", data, { icon = icon .. pad })
+    end
 
-for name, data in pairs(nvchad_devicons) do
-    local icon = data.icon
-    local pad = vim.api.nvim_strwidth(icon) == 1 and " " or ""
-    override[name] = vim.tbl_extend("force", data, { icon = icon .. pad })
+    for name, data in pairs(nvchad_devicons) do
+        local icon = data.icon
+        local pad = vim.api.nvim_strwidth(icon) == 1 and " " or ""
+        devicons[name] = vim.tbl_extend("force", data, { icon = icon .. pad })
+    end
 end
 
 return {
     {
         "nvim-tree/nvim-tree.lua",
-        config = function()
-            require("nvim-tree").setup({
-                renderer = {
-                    icons = {
-                        git_placement = "after",
-                        padding = "",
-                        symlink_arrow = " ➜ ",
-                        glyphs = {
-                            default = " ",
-                            symlink = " ",
-                            bookmark = "󰆤 ",
-                            modified = "●",
-                            folder = {
-                                arrow_closed = "",
-                                arrow_open = "",
-                                default = " ",
-                                open = " ",
-                                empty = " ",
-                                empty_open = " ",
-                                symlink = " ",
-                                symlink_open = " ",
-                            },
-                            git = {
-                                unstaged = "✗",
-                                staged = "✓",
-                                unmerged = "",
-                                renamed = "➜",
-                                untracked = "+",
-                                deleted = "-",
-                                ignored = "◌",
-                            },
-                        },
-                    },
-                },
-            })
+        opts = nvtree_opts,
+        config = function(_, opts)
+            dofile(vim.g.base46_cache .. "nvimtree")
+            require("nvim-tree").setup(opts)
         end,
     },
 
     {
         "nvim-tree/nvim-web-devicons",
-        config = function()
+        opts = devicons,
+        config = function(_, opts)
             dofile(vim.g.base46_cache .. "devicons")
-            require("nvim-web-devicons").setup({
-                override = override,
-            })
+            require("nvim-web-devicons").setup(opts)
         end,
     },
 }
